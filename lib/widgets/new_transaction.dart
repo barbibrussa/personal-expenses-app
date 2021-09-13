8b10471191ddaa4549abdf3deaad260bacf6f96d
barbibrussa <barbibrussa@gmail.com>
2021-09-13 15:01:23 -0300

    Implement scroll view on new transaction widget

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -52,49 +52,56 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              onSubmitted: (_) => _submitData,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData,
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    _selectedDate == null
-                        ? 'No chosen date!'
-                        : 'Picked date ${DateFormat.yMd().format(_selectedDate)}',
-                  ),
-                ),
-                FlatButton(
-                    textColor: Theme.of(context).primaryColor,
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: titleController,
+                onSubmitted: (_) => _submitData,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
                     child: Text(
-                      'Choose date',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                      _selectedDate == null
+                          ? 'No chosen date!'
+                          : 'Picked date ${DateFormat.yMd().format(_selectedDate)}',
                     ),
-                    onPressed: _presentDatePicker)
-              ],
-            ),
-            RaisedButton(
-              child: Text('Add a transaction'),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-              onPressed: _submitData,
-            ),
-          ],
+                  ),
+                  FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text(
+                        'Choose date',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: _presentDatePicker)
+                ],
+              ),
+              RaisedButton(
+                child: Text('Add a transaction'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+                onPressed: _submitData,
+              ),
+            ],
+          ),
         ),
       ),
     );
